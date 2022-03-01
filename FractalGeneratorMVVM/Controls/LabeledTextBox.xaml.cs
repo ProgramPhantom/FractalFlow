c41330398242a9efedf53339d5949895b2582e24
ProgramPhantom <49105496+ProgramPhantom@users.noreply.github.com>
2022-03-01 17:27:48 +0000

    Fractal Frame and Painter Window

---
+++ FractalGeneratorMVVM/Controls/LabeledTextBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FractalGeneratorMVVM.Controls
+{
+    public partial class LabeledTextBox : UserControl
+    {
+        public LabeledTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
