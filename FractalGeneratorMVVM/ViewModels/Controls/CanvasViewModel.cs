1ea87e0a36bbb0c01f5a3d076dbe3b0bb455c347
ProgramPhantom <49105496+ProgramPhantom@users.noreply.github.com>
2022-05-08 17:59:59 +0100

    You can now get the mouse position in the canvas View Model

--- FractalGeneratorMVVM/ViewModels/Controls/CanvasViewModel.cs
+++ FractalGeneratorMVVM/ViewModels/Controls/CanvasViewModel.cs
@@ -5,12 +5,14 @@ using System.Text;
 using FractalCore;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using System.Windows.Input;
+using System.Windows;
 
 namespace FractalGeneratorMVVM.ViewModels.Controls
 {
     public class CanvasViewModel : Screen
     {
-
+        
         private FractalImage? _image;
 
         public FractalImage? Image
@@ -23,9 +25,20 @@ namespace FractalGeneratorMVVM.ViewModels.Controls
             }
         }
 
+
+
         public CanvasViewModel()
         {
             
+        }
+
+        public void MouseOver( System.Windows.Controls.Image sender, MouseEventArgs relative)
+        {
+            // pos = Mouse.GetPosition(relative.View);
+            Point pos = Mouse.GetPosition(sender);
+            
+
+            System.Diagnostics.Trace.WriteLine($"X: {pos.X}, Y: {pos.Y}");
         }
     }
 }
